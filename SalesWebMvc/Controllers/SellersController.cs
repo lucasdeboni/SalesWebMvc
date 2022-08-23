891153aa81920d5521b6b77a4f2099cd37b853f9
Lucas Deboni <deboni.lucas@gmail.com>
2022-08-22 21:25:40 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -17,6 +18,19 @@ namespace SalesWebMvc.Controllers
     {
       var list = _sellerService.FindAll();
       return View(list);
+    }
+
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Seller seller)
+    {
+      _sellerService.Insert(seller);
+      return RedirectToAction(nameof(Index));
     }
   }
 }
